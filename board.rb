61511c165439df3bef08f4930e08b5ca7ca08955
Mark Azemoon <mmazemoon@gmail.com>
2015-04-30 15:29:33 -0700

    allow piece objects to calculate & return possible moves

--- board.rb
+++ board.rb
@@ -1,4 +1,17 @@
+require './game.rb'
+require './piece.rb'
+require './player.rb'
+
 class Board
+  # piece.rb
+  # @board[*@current_position]
+  # @board[2, 1]
+
+  # [] pos_arr
+  # @grid[pos[0]][pos[1]] => @board[[2,1]]
+  def [](position)
+    @grid[pos[0]][pos[1]]   #
+  end
 
   def initialize
     @grid = Array.new(8) { Array.new(8) }  # nil is immutable
@@ -10,22 +23,47 @@ class Board
   # row index + col index = odd ? put a piece there
 
   def populate_board
-    grid_design = { red: [0, 1, 2], white: [5, 6, 7] }
+    grid_design = { red: [0, 1, 2], wht: [5, 6, 7] }
 
     grid_design.each do |key, val|
       val.each do |row_num|
         if row_num.even?
-          [1, 3, 5, 7].each do |odd_col|
-            @grid[row_num][col_num] = Piece.new(self,[row_num, odd_col], key)
+          [1, 3, 5, 7].each do |col_num|
+            @grid[row_num][col_num] = Piece.new(self,[row_num, col_num], key)
           end
         else
-          [0, 2, 4, 6].each do |even_col|
-            @grid[row_num][odd_col] = Piece.new(self,[row_num, odd_col], key)
+          [0, 2, 4, 6].each do |col_num|
+            @grid[row_num][col_num] = Piece.new(self,[row_num, col_num], key)
           end
         end
+      end
+    end
     nil
   end
 
+  def display
+    render_string = ""
+    @grid.each.with_index do |row, idx1|
+      row.each.with_index do |piece, idx2|
+        if piece.is_a?(Piece)
+          render_string << " #{piece.color} "
+        else
+          render_string << "  -  "
+        end
+      end
+      render_string << "\n"
+    end
+    print render_string
+  end
 
+  def move_piece(start, finish)
+    piece = self[start_pos]
+    piece.perform_slide(finish)
+  end
+end
 
+if __FILE__ == $PROGRAM_NAME
+  board = Board.new
+  board.populate_board
+  board.display
 end
--- piece.rb
+++ piece.rb
@@ -1,4 +1,9 @@
+NORMAL_DIR = { red: [[1,1], [1,-1]], wht: [[-1,1], [-1,-1]] }
+KILLING_DIR = { red: [[2,2], [2, -2]], wht: [[-2,2], [-2,-2]] }
+
+require 'byebug'
 class Piece
+  attr_reader :color
 
   def initialize(board, position, color, king = false)
     @board = board
@@ -7,9 +12,9 @@ class Piece
     @king = king
   end
 
-  def perform_slide(start, finish)
-  
   # call a valid moves
+  def perform_slide(finish)
+    valid_moves
 
   end
 
@@ -17,8 +22,35 @@ class Piece
 
   end
 
+  # possible and legal moves
   def valid_moves
+    row = @current_position[0]
+    col = @current_position[1]
+    result = []
+    dir_arr = direction
+    dir_arr.each.with_index do |direction|
+      next_move = [row + direction[0], col + direction[1]]
+      result << next_move if on_board?(next_move)
+    end
+    result
+  end
 
+  def on_board?(pos)
+    row, col = pos[0], pos[1]
+    if row >= 0 && row <= 7
+      if col >= 0 && col <= 7
+        return true
+      end
+    end
+    false
+  end
+
+  def direction
+    possible_moves = NORMAL_DIR[@color] + KILLING_DIR[@color]
+    
   end
 
 end
+
+piece = Piece.new(nil, [3, 3], :wht)
+p piece.valid_moves
